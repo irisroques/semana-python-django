2e2318eefa6587c2fb33cc92ffdd44d308e04bfb
Iris Roques <irisroques@yandex.com>
2020-05-01 22:54:21 -0300

    31:47 video 2

--- app/views.py
+++ app/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-
+from .forms import TarefaForm
 # Create your views here.
 
 def listar_tarefas(request):
     return render(request, 'tarefas/listar_tarefas.html')
+
+def cadastrar_tarefa(request):
+    form_tarefa = TarefaForm()
+    return render(request, 'tarefas/form_tarefa.html',{"form_tarefa": form_tarefa})
